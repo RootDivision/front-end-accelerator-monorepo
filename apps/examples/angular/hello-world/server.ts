fc4bed484e5e5bc53cdd9f0850f27aae6a01dbd4
Vuong Ha <vuong.ha@cegeka.com>
2025-01-30 11:42:02 +0100

    Add ngrx

--- apps/examples/angular/hello-world/server.ts
+++ apps/examples/angular/hello-world/server.ts
@@ -1,8 +1,9 @@
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr';
 import express from 'express';
-import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+
 import bootstrap from './src/main.server';
 
 // The Express app is exported so that it can be used by serverless Functions.
@@ -21,21 +22,21 @@ export function app(): express.Express {
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
   server.get('**', express.static(browserDistFolder, {
-    maxAge: '1y',
     index: 'index.html',
+    maxAge: '1y',
   }));
 
   // All regular routes use the Angular engine
   server.get('**', (req, res, next) => {
-    const { protocol, originalUrl, baseUrl, headers } = req;
+    const { baseUrl, headers, originalUrl, protocol } = req;
 
     commonEngine
       .render({
         bootstrap,
         documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${originalUrl}`,
-        publicPath: browserDistFolder,
         providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
+        publicPath: browserDistFolder,
+        url: `${protocol}://${headers.host}${originalUrl}`,
       })
       .then((html) => res.send(html))
       .catch((err) => next(err));
