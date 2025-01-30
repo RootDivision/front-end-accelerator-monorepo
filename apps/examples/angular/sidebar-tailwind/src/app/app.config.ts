8c657756bc85e8c5236a76d39f08eb5d1182136a
Vuong Ha <vuong.ha@cegeka.com>
2025-01-30 12:03:49 +0100

    adfsdfg

--- apps/examples/angular/sidebar-tailwind/src/app/app.config.ts
+++ apps/examples/angular/sidebar-tailwind/src/app/app.config.ts
@@ -1,16 +1,8 @@
-import {
-  ApplicationConfig,
-  provideExperimentalZonelessChangeDetection,
-} from '@angular/core';
-import { provideClientHydration } from '@angular/platform-browser';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideExperimentalZonelessChangeDetection(),
-    provideRouter(routes),
-    provideClientHydration(),
-  ],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
