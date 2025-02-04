6e91e0d02e85ea2bec7b0c99db643441455d7e60
Vuong Ha <vuong.ha@cegeka.com>
2025-02-04 21:19:21 +0100

    asdasd

--- apps/demo/next/examples/next-open-ai-chat/app/api/chat/route.ts
+++ apps/demo/next/examples/next-open-ai-chat/app/api/chat/route.ts
@@ -11,6 +11,7 @@ interface ChatMessage {
 }
 
 export async function POST(req: Request) {
+  console.log('hahah', process.env.OPENAI_API_KEY);
   const { messages } = (await req.json()) as { messages: ChatMessage[] };
 
   const result = streamText({
